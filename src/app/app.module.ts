77fadb56552d4a497c64c8662278c89fbbb471aa
Michal Stejskal <stejsmi5@fit.cvut.cz>
2020-09-07 10:18:54 +0200

    update project dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,50 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
-import {AppComponent} from './app.component';
-import {routingModule} from './app.routing';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ReactiveFormsModule, FormGroup, FormsModule} from '@angular/forms';
-
-import {
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule, MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-} from '@angular/material';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {HttpErrorHandler} from './http-error-handler.service';
-import {MessageService} from './message.service';
-import {AutosizeModule} from 'ngx-autosize';
-import {ItemListComponent} from './counter/item-list/item-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AppComponent } from './app.component';
+import { routingModule } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormGroup, FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpErrorHandler } from './http-error-handler.service';
+import { MessageService } from './message.service';
+import { AutosizeModule } from 'ngx-autosize';
+import { ItemListComponent } from './counter/item-list/item-list.component';
 import { ItemAddComponent } from './counter/item-add/item-add.component';
+import { NgModule } from '@angular/core';
 
 
 @NgModule({
@@ -65,7 +61,6 @@ import { ItemAddComponent } from './counter/item-add/item-add.component';
     HttpClientModule,
     MatAutocompleteModule,
     MatBadgeModule,
-    MatBottomSheetModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
@@ -73,7 +68,6 @@ import { ItemAddComponent } from './counter/item-add/item-add.component';
     MatChipsModule,
     MatDatepickerModule,
     MatDialogModule,
-    MatDividerModule,
     MatExpansionModule, MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
-import {RouterModule, Routes} from '@angular/router';
-import {ModuleWithProviders} from '@angular/core';
-import {ItemListComponent} from './counter/item-list/item-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { ItemListComponent } from './counter/item-list/item-list.component';
 import { ItemAddComponent } from './counter/item-add/item-add.component';
 
 const routes: Routes = [
-  {path: '', component: ItemListComponent},
-  {path: 'add', component: ItemAddComponent},
+  { path: '', component: ItemListComponent },
+  { path: 'add', component: ItemAddComponent },
 ];
 
-export const routingModule: ModuleWithProviders = RouterModule.forRoot(routes);
+export const routingModule: ModuleWithProviders<unknown> = RouterModule.forRoot(routes);
